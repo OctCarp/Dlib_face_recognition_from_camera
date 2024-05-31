86ceeaaee99ab0d7157cb48d4fe9ff7d77621f7a
OctCarp <12110304@mail.sustech.edu.cn>
2024-05-31 16:22:10 +0800

    get_faces_from_camera_tkinter.py

--- get_faces_from_camera_tkinter.py
+++ get_faces_from_camera_tkinter.py
@@ -25,22 +25,21 @@ detector = dlib.get_frontal_face_detector()
 
 # GStreamer 管道 / GStreamer pipeline
 def gstreamer_pipeline(
-    capture_width=1280,
-    capture_height=720,
-    display_width=1280,
-    display_height=720,
-    framerate=60,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
     flip_method=0,
 ):
     return (
         "nvarguscamerasrc ! "
         "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
         "nvvidconv flip-method=%d ! "
         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
         "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
+        "video/x-raw, format=(string)BGR ! appsink drop=True"
         % (
             capture_width,
             capture_height,
